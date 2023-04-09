07f3e844e8139a6b6c4337ffb037616a74634cd5
Ausra <munduswebstudio@gmail.com>
2023-04-09 12:28:14 +0300

    Done

--- js/07-uzduotis.js
+++ js/07-uzduotis.js
@@ -11,15 +11,23 @@ Ir t.t.iki
 10 * 3 = 30
 */
 
-const a = 3;
 
-for (let i = 1; i <=10; i = i + 1) {
-    console.log(i * a);
+/*
+const skaicius = 3;
+
+for (let i = 1; i <= 10; i = i + 1) {
+    console.log(i * skaicius);
 }
+*/
 
-function Multiplications (a) {
-    for (let i = 1; i <=10; i = i + 1) {
-        return i * a;
+console.log('-------------------');
+
+function Multiplications (skaicius) {
+    let rezultatas = '';
+    for (let i = 1; i <= 10; i++) {
+        rezultatas = rezultatas + `${i} * ${skaicius} = ${i*skaicius} \n\r`;
     }
+    return rezultatas;
 }
+
 console.log(Multiplications(3));
